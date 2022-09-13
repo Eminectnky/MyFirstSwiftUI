dbca345dc3d43ce0cd31e3530a9204471b8f1697
Emine Çetinkaya <eminecetinkayaa98@gmail.com>
2022-09-13 13:58:51 +0300

    Initial Commit

---
+++ MyFirstSwiftUI/MyFirstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstSwiftUIApp.swift
+//  MyFirstSwiftUI
+//
+//  Created by Emine CETINKAYA on 13.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
