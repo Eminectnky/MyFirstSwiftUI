e580326206d7e738d1c9695eb0df4de999a24325
Emine <74454368+Eminectnky@users.noreply.github.com>
2022-09-15 00:35:20 +0300

    project updated

--- MyFirstSwiftUI/Game.swift
+++ MyFirstSwiftUI/Game.swift
@@ -5,6 +5,8 @@
 //  Created by Emine CETINKAYA on 14.09.2022.
 // new project created
 
+//project updated
+
 import Foundation
 
 class Game : NSObject {
@@ -20,6 +22,39 @@ class Game : NSObject {
         self.priceInDollars = priceInDollars
         self.serialNumber = serialNumber
         self.dateCreated = Date()
+        
+        
+    }
+    
+    convenience init (random : Bool = false){
+        if random {
+            let conditions = ["New", "Mint", "Used"]
+            
+            var idx =  arc4random_uniform(UInt32(conditions.count))
+            
+            let randomCoundation = conditions[Int(idx)]
+            
+           let names = ["Resident Evil", "Gears Of War", "Halo", "God of War"]
+            
+            idx =  arc4random_uniform(UInt32(names.count))
+            
+            let randomName = names[Int(idx)]
+            
+            idx =  arc4random_uniform(UInt32(6))
+            
+            let randomTitle = "\(randomCondition) \(randomName) \(Int(idx))"
+            
+            let serialNumber = UUID().uuidString.components(separatedBy: "-").first!
+            
+            let priceInDollars =
+            
+            idx = UInt32(Double(arc4random_uniform(UInt32(70))))
+            
+            self.init(name : randomTitle , priceInDollars : priceInDollars , serialNumber : serialNumber)
+            
+        }else{
+            self.init(name : "" , priceInDollars : 0 , serialNumber : "")
+        }
         
         
     }
