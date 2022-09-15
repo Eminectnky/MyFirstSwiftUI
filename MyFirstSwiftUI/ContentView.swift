2ba9a9e5c478c7f69488605b3743915e879e98cb
Emine <74454368+Eminectnky@users.noreply.github.com>
2022-09-16 02:24:33 +0300

    new project created

--- MyFirstSwiftUI/ContentView.swift
+++ MyFirstSwiftUI/ContentView.swift
@@ -4,57 +4,24 @@
 //
 //  Created by Emine CETINKAYA on 13.09.2022.
 
+//new project created
+
 
 import SwiftUI
 
 
-
 struct ContentView: View {
-    
-    let numberFormatter : NumberFormatter = {
-        
-        let formatter = NumberFormatter()
-        formatter.numberStyle = .currency
-        formatter.currencySymbol = "$"
-        formatter.currencyCode = "USD"
-        formatter.minimumFractionDigits = 0
-        formatter.maximumFractionDigits = 2
-        return formatter
-        
-    }()
-    
-    let gameStore = GameStore()
     var body: some View {
-        List(gameStore.games){ (game) in
-            
-            HStack{
-                
-            
-                VStack(alignment:.leading, spacing: 4.0){
-                Text(game.name)
-                      .font(.body)
-                Text(game.serialNumber)
-                        .font(.caption)
-                        .foregroundColor(Color(white: 0.65))
-                
-            }
-                Spacer()
-                Text(NSNumber(value: game.priceInDollars), formatter: numberFormatter)
-                Text("\(game.priceInDollars)")
-                    .font(.title2)
-                    .foregroundColor(game.priceInDollars > 30 ? .blue: .gray)
-            }
-            .padding(.vertical , 6)
-            
-         
+        HStack {
+            VStack {
+          
         }
-   
     }
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
 }
