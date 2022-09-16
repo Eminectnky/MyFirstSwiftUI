9afc3041cf34234e660e062e7032b044b6c8281a
Emine <74454368+Eminectnky@users.noreply.github.com>
2022-09-16 12:36:17 +0300

    gradient added

--- MyFirstSwiftUI/ContentView.swift
+++ MyFirstSwiftUI/ContentView.swift
@@ -5,23 +5,24 @@
 //  Created by Emine CETINKAYA on 13.09.2022.
 
 //new project created
-
+// gradient added
 
 import SwiftUI
 
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            VStack {
-          
+        ZStack {
+            Color(.blue)
+            LinearGradient(gradient: Gradient(colors:[.blue, . white]),
+                           startPoint: .topLeading, endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
+    static var previews: some View{
         ContentView()
     }
-}
 }
