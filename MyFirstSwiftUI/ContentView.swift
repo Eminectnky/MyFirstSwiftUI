091cc912bf47219e3c75a5cc0cb52d4b8043beb2
Emine <74454368+Eminectnky@users.noreply.github.com>
2022-09-17 12:41:25 +0300

    day added

--- MyFirstSwiftUI/ContentView.swift
+++ MyFirstSwiftUI/ContentView.swift
@@ -10,6 +10,8 @@
 //SF Symbols
 //Main VStack
 //sf symbol loaded and image added
+//HStack of Days
+//day added
 
 import SwiftUI
 
@@ -28,14 +30,36 @@ struct ContentView: View {
                     .foregroundColor(.white)
                     .padding()
                 
-                VStack{
+                VStack(spacing: 10){
                     
                     Image(systemName: "cloud.sun.fill")
                         .renderingMode(.original)
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .frame(width: 100, height: 100)
+                        .frame(width: 180, height: 180)
                     Text("76°")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundColor(.white)
+                }
+                
+                HStack{
+                    VStack{
+                        Text("TUE")
+                            .font(.system(size: 16, weight: .medium, design: .default))
+                            .foregroundColor(.white)
+                        
+                        Image(systemName: "cloud.sun.fill")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 40, height: 40)
+                        
+                        Text("76°")
+                            .font(.system(size: 28, weight: .medium))
+                            .foregroundColor(.white)
+                    }
+                    
+                    
                 }
                 Spacer()
                   
