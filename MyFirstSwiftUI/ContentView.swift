b5236e41fe14c3523d325285f520bc3aa33d6f8e
Emine <74454368+Eminectnky@users.noreply.github.com>
2022-09-17 10:13:15 +0300

    sf symbol loaded and image added

--- MyFirstSwiftUI/ContentView.swift
+++ MyFirstSwiftUI/ContentView.swift
@@ -7,6 +7,9 @@
 //new project created
 // gradient added
 //added city text in VStack
+//SF Symbols
+//Main VStack
+//sf symbol loaded and image added
 
 import SwiftUI
 
@@ -24,6 +27,16 @@ struct ContentView: View {
                     .font(.system(size: 32, weight: .medium, design: .default))
                     .foregroundColor(.white)
                     .padding()
+                
+                VStack{
+                    
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100, height: 100)
+                    Text("76°")
+                }
                 Spacer()
                   
                 
