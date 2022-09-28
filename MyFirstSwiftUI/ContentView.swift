909bcf0053cafb1f16c2369589d34db97e087a3d
Emine <74454368+Eminectnky@users.noreply.github.com>
2022-09-28 14:04:39 +0300

    Hello,world!

--- MyFirstSwiftUI/ContentView.swift
+++ MyFirstSwiftUI/ContentView.swift
@@ -1,165 +1,38 @@
-//
-//  ContentView.swift
-//  MyFirstSwiftUI
-//  Created by Emine CETINKAYA on 13.09.2022.
-//added city text in VStack
-//SF Symbols
-//Main VStack
-//sf symbol loaded and image added
-//HStack of Days
-//day added
-//Main VStack updated
-//HSatck Of Days updated
-//Button & Refactor added
-//Button & Refactor updated
-//State & @Binding
-//@Binding
-
+//Hello,world!
 import SwiftUI
+
 struct ContentView: View {
     
-    @State private var isNight = false
-    
+    @State var isTurkish: Bool = false
     var body: some View {
-        ZStack {
-            BackgroundView(isNight: $isNight)
+        VStack{
             
-            VStack{
-               CityTextView(cityName: "Cupertino, CA")
+            if isTurkish{
+                Text("Merhaba dünya!")
                 
-                MainWeatherStatusView(imageName: isNight ? "moon.stars.fill" : "cloud.sun.fill", temperature: 76)
+            } else {
+                Text("Hello, world!")
                 
-                HStack(spacing: 20){
+            }
+            
+            Button {
                 
-                  WeatherDayView(dayOfWeek: "TUE", imageName: "cloud.sun.fill", temperature: 74)
-                    
-                    
-                    WeatherDayView(dayOfWeek: "WED", imageName: "sun.max.fill", temperature: 88)
-                    
-                    
-                    
-                    WeatherDayView(dayOfWeek: "THU", imageName: "wind.snow",
-                        temperature: 55)
-                    
-                    
-                    WeatherDayView(dayOfWeek: "FRI", imageName: "sunset.fill", temperature: 60)
-                
-                WeatherDayView(dayOfWeek: "SAT", imageName: "snowflake", temperature: 25)
-                    
-               
-                
-                }
-                Spacer()
-                
-                Button{
-                    isNight.toggle()
+                self.isTurkish.toggle()
                 
             } label: {
-                Text("Change Day Time")
-                    .frame(width: 280, height: 50)
-                    .background(Color.white)
-                    .font(.system(size: 20, weight: .bold, design: .default))
-                    .cornerRadius(10)
-           
+                Text(isTurkish ? "Hello" : "Merhaba")
+                
             }
-             Spacer()
+            
         }
+        .font(.title)
+        
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View{
+    static var previews: some View {
         ContentView()
     }
 }
-
-struct WeatherDayView : View{
-    
-    var dayOfWeek: String
-    var imageName : String
-    var temperature :Int
-        
-    var body: some View {
-        VStack {
-            
-          Text(dayOfWeek)
-             .font(.system(size: 16, weight: .medium, design: .default))
-             .foregroundColor(.white)
-                        
-          Image(systemName: imageName)
-             .renderingMode(.original)
-             .resizable()
-             .aspectRatio(contentMode: .fit)
-             .frame(width: 40, height: 40)
-                        
-          Text("\(temperature)°")
-             .font(.system(size: 28, weight: .medium))
-             .foregroundColor(.white)
-        }
-    }
-}
-                    
-      
-                             struct BackgroundView : View {
-                                 
-                             @Binding var isNight: Bool
-                                 
-                                var body: some View {
-                                
-                                
-                               
-                                
-                                    LinearGradient(gradient: Gradient(colors: [isNight ? .black : .blue, isNight ? .gray: Color("lightBlue")]),
-                                               
-                                               startPoint: .topLeading,
-                                               endPoint: .bottomTrailing)
-                                .edgesIgnoringSafeArea(.all)
-    }
-}
-    struct CityTextView: View {
-        var cityName : String
-        
-        
-        var body: some View {
-                                
-        Text(cityName)
-                                    
-            .font(.system(size: 32, weight: .medium, design: .default))
-                                    
-            .foregroundColor(.white)
-                                    
-            .padding()
-            
-        }
-        
-    }
-    
-}
-
-struct MainWeatherStatusView: View {
-    
-    var imageName: String
-    var temperature: Int
-    
-    var body: some View {
-        VStack(spacing: 10){
-            Image(systemName: imageName)
-                .renderingMode(.original)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 180, height: 180)
-            Text("\(temperature)°")
-                .font(.system(size: 70, weight: .medium))
-                .foregroundColor(.white)
-        }
-        
-        .padding(.bottom, 40)
-    }
-}
-                            
-                            
-    
-    
-
-
-
